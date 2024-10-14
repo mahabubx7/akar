7859db36ed2268a618a84be470c85ec5d77f4c36
Mahabub <mahabubx7@gmail.com>
2024-10-14 21:45:29 +0600

    refactor(boolean): added exact rule

--- lib/schemas/__tests__/boolean.test.ts
+++ lib/schemas/__tests__/boolean.test.ts
@@ -2,20 +2,29 @@ import { describe, expect, test } from "vitest"
 import { AkarBoolean } from "../boolean"
 
 describe("Test schema: Boolean", () => {
-  const boolean = new AkarBoolean()
+  const boolean = () => new AkarBoolean()
 
   test("should return data after validate passes", () => {
-    const schema = boolean
+    const schema = boolean()
     expect(schema.parse(true).value).toBeDefined()
     expect(schema.parse(true).value!).toBe(true)
   })
 
   test("should return error when value is not a boolean", () => {
-    const schema = boolean
+    const schema = boolean()
     expect(schema.parse(5).errors).toBeDefined()
     expect(schema.parse(5).errors?.length).toBeGreaterThanOrEqual(1)
     expect(schema.parse(5).errors![0].reason).toContain(
       "Invalid type, expected boolean"
+    )
+  })
+
+  test("should return error when value is not equal to exact value", () => {
+    const schema = boolean().exact(true)
+    expect(schema.parse(false).errors).toBeDefined()
+    expect(schema.parse(false).errors?.length).toBeGreaterThanOrEqual(1)
+    expect(schema.parse(false).errors![0].reason).toContain(
+      "Value must be true"
     )
   })
 })
--- lib/schemas/boolean.ts
+++ lib/schemas/boolean.ts
@@ -9,21 +9,37 @@
 import { AkarBase } from "./base"
 
 export class AkarBoolean extends AkarBase<boolean> {
+  private exactValue: boolean | null = null
+
+  exact(value: boolean): this {
+    this.exactValue = value
+    return this
+  }
+
   parse(input: any): {
     value?: boolean
     errors?: { field: string; reason: string; value?: any }[]
   } {
+    const errors: { field: string; reason: string; value?: any }[] = []
     if (typeof input !== "boolean") {
-      return {
-        errors: [
-          {
-            field: "boolean",
-            reason: "Invalid type, expected boolean",
-            value: input
-          }
-        ]
-      }
+      errors.push({
+        field: "boolean",
+        reason: "Invalid type, expected boolean",
+        value: input
+      })
     }
+    if (this.exactValue !== null && input !== this.exactValue) {
+      errors.push({
+        field: "boolean",
+        reason: `Value must be ${this.exactValue}`,
+        value: input
+      })
+    }
+
+    if (errors.length > 0) {
+      return { errors }
+    }
+
     return { value: input }
   }
 }
