2063db48f833d57422ba41d3e616919fa45d2a15
Mahabub <mahabubx7@gmail.com>
2024-10-15 16:34:01 +0600

    fix(release-it): release-it cli with conventional commits

--- release-it.config.js
+++ release-it.config.js
@@ -18,8 +18,24 @@ module.exports = {
   },
   plugins: {
     "@release-it/conventional-changelog": {
-      preset: "angular",
       infile: "CHANGELOG.md",
+      preset: {
+        name: "conventionalcommits",
+        types: [
+          { type: "feat", section: "Features" },
+          { type: "fix", section: "Bug Fixes" },
+          { type: "perf", section: "Performance Improvements" },
+          { type: "revert", section: "Reverts" },
+          { type: "docs", section: "Documentation" },
+          { type: "style", section: "Styles" },
+          { type: "refactor", section: "Code Refactoring" },
+          { type: "test", section: "Tests" },
+          { type: "build", section: "Build System" },
+          { type: "ci", section: "Continuous Integration" },
+          { type: "chore", section: "Chores" },
+          { type: "breaking", section: "Breaking Changes", hidden: true }
+        ]
+      },
       whatBump: (commits) => {
         let level = 2
         let reason = "0 breaking changes, 0 features"
